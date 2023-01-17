41b4cf73c0c7f89c22b374dd661dee35465a8463
Roman <romariiiii@gmail.com>
2023-01-17 16:49:25 +0530

    chapter5Less3 знакомство с react-hook-form

--- src/chapter5Less1/App.js
+++ src/chapter5Less1/App.js
@@ -2,54 +2,12 @@ import React from "react";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import Stack from "@mui/material/Stack";
+import { useForm } from "react-hook-form";
 
 const App = () => {
-  const [fields, setFields] = React.useState({
-    firstName: "",
-    lastName: "",
-    email: "",
-    password: "",
-  });
+  const { register, handleSubmit, watch, formState } = useForm();
+  const onSubmit = (data) => console.log('ФОРМА', data);
 
-  const onChangeFields = (event) => {
-    setFields({
-      ...fields,
-      [event.target.name]: event.target.value,
-    });
-  };
-
-  const handleClickClear = () => {
-    setFields({
-      firstName: "",
-      lastName: "",
-      email: "",
-      password: "",
-    });
-  };
-
-  const onClickRegistr = () => {
-    //console.log(fields.firstName.length);
-    const valid =
-      (fields.firstName.length > 2 || fields.lastName.length > 2) &&
-      fields.email.include("@") &&
-      fields.password.length > 5;
-    if (!(fields.firstName.length > 1 && fields.lastName.length > 1)) {
-      alert("Поля имя и фамилия должны содержать более одного символа!");
-    } else if (!fields.email.includes("@")) {
-      alert("Поле Email должно содержать символ @ !");
-    } else if (!(fields.password.length > 5)) {
-      alert("Пароль должен быть более 5 символов!");
-    } else {
-      console.log(fields);
-      handleClickClear();
-    }
-  };
-
-  const isValid =
-    !!fields.firstName &&
-    !!fields.lastName &&
-    !!fields.email &&
-    !!fields.password;
   return (
     <div className="App">
       <Stack
@@ -60,45 +18,62 @@ const App = () => {
         width="500px"
       >
         <TextField
-          value={fields.firstName}
-          onChange={onChangeFields}
+          {...register("firstName", {
+            required: "Это обязательное поле!",
+          })}
+          helperText={
+            formState.errors.firstName && formState.errors.firstName.message
+          }
+          error={!!formState.errors.firstName}
           label="Имя"
           fullWidth
           name="firstName"
         />
         <TextField
-          value={fields.lastName}
-          onChange={onChangeFields}
+          {...register("lastName", {
+            required: "Это обязательное поле!",
+          })}
+          helperText={
+            formState.errors.lastName && formState.errors.lastName.message
+          }
+          error={!!formState.errors.lastName}
           label="Фамилия"
           fullWidth
           name="lastName"
         />
         <TextField
-          value={fields.email}
-          onChange={onChangeFields}
-          type="email"
+          {...register("email", {
+            required: "Это обязательное поле!",
+            pattern: {
+              value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+              message: "Неверно указан Email!",
+            },
+          })}
+          helperText={formState.errors.email && formState.errors.email.message}
+          error={!!formState.errors.email}
+          //type="email"
           label="Email"
           fullWidth
           name="email"
         />
         <TextField
-          value={fields.password}
-          onChange={onChangeFields}
+          {...register("password", {
+            required: "Это обязательное поле!",
+          })}
+          helperText={
+            formState.errors.password && formState.errors.password.message
+          }
+          error={!!formState.errors.password}
           type="password"
           label="Пароль"
           fullWidth
           name="password"
         />
 
-        <Button
-          disabled={!isValid}
-          onClick={onClickRegistr}
-          fullWidth
-          variant="contained"
-        >
+        <Button onClick={handleSubmit(onSubmit)} fullWidth variant="contained">
           Зарегистрироваться
         </Button>
-        <Button onClick={handleClickClear} fullWidth variant="outlined">
+        <Button fullWidth variant="outlined">
           Очистить
         </Button>
       </Stack>
